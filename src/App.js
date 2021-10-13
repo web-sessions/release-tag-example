d06accb933c1c9b810f27d232b55b84a1c8a601d
Luis Meneses <luismenesesep@gmail.com>
2021-10-13 00:41:48 -0500

    [AAA-000] - Style added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="App-p">
           Hello everyone!
         </p>
       </header>
