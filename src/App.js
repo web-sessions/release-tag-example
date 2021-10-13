6a3e0dfbfdceceea9e11231a44d4e2f25413d510
Luis Meneses <luismenesesep@gmail.com>
2021-10-13 00:47:35 -0500

    Aaa 000 (#3) (#4)
    * [AAA-000] - text fixed
    
    * [AAA-000] - Style added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello!
+        <p className="App-p">
+          Hello everyone!
         </p>
       </header>
     </div>
